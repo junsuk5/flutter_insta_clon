bf17fd3b3c9dbb785eee9ca1c7a241533ead464c
Eddie Moon <intelliemoon@gmail.com>
2019-01-27 00:58:18 +0900

    ImagePicker로 이미지 불러오기.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,8 +9,49 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  File galleryFile;
+
+//save the result of camera file
+  File cameraFile;
+
   @override
   Widget build(BuildContext context) {
-    return Text('1 page');
+    //display image selected from gallery
+    imageSelectorGallery() async {
+      galleryFile = await ImagePicker.pickImage(
+        source: ImageSource.gallery,
+        // maxHeight: 50.0,
+        // maxWidth: 50.0,
+      );
+      print("You selected gallery image : " + galleryFile.path);
+      setState(() {});
+    }
+
+    return new Scaffold(
+      body: new Builder(
+        builder: (BuildContext context) {
+          return new Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              new RaisedButton(
+                child: new Text('Select Image from Gallery'),
+                onPressed: imageSelectorGallery,
+              ),
+              displaySelectedFile(galleryFile),
+            ],
+          );
+        },
+      ),
+    );
+  }
+
+  Widget displaySelectedFile(File file) {
+    return new SizedBox(
+      height: 200.0,
+      width: 300.0,
+      child: file == null
+          ? new Text('Sorry nothing selected!!')
+          : new Image.file(file),
+    );
   }
 }
