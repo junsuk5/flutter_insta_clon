f9b1800272b9af22d77c87317f6608340b4cb95f
Junsuk Oh <a811219@gmail.com>
2019-02-03 23:05:50 +0900

    Search 탭 UI 수정

--- lib/search_page.dart
+++ lib/search_page.dart
@@ -8,13 +8,37 @@ class SearchPage extends StatefulWidget {
 class _SearchPageState extends State<SearchPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            '𝔦𝔫𝔰𝔱𝔞𝔤𝔯𝔞𝔪 𝔠𝔩𝔬𝔫',
-            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
-          ),
-        ),
-        body: Text('Search page'));
+    return Scaffold(appBar: _buildAppBar(), body: _buildBody());
+  }
+
+  Widget _buildAppBar() {
+    return AppBar(
+      title: Text(
+        '𝔦𝔫𝔰𝔱𝔞𝔤𝔯𝔞𝔪 𝔠𝔩𝔬𝔫',
+        style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+
+  Widget _buildBody() {
+    return GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            childAspectRatio: 1.0,
+            mainAxisSpacing: 1.0,
+            crossAxisSpacing: 1.0),
+        itemCount: 50,
+        itemBuilder: (BuildContext context, int index) {
+          return _buildListItem(context, index);
+        });
+  }
+
+  Widget _buildListItem(context, index) {
+    return InkWell(
+      child: Image.network(
+        'https://cdn.pixabay.com/photo/2018/11/29/21/19/hamburg-3846525_1280.jpg',
+        fit: BoxFit.cover,
+      ),
+    );
   }
 }
